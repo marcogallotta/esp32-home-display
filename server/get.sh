f2f19669d6e6489c61a3717e8dd6cf4196dc5849
Marco Gallotta <marco@gallotta.co.za>
2026-04-22 17:33:23 +0200

    API: del get.sh

--- server/get.sh
+++
@@ -1,5 +0,0 @@
-curl -sS -H "X-api-key: $(python3 -c 'from config import load_config; print(load_config()["api_key"])')" \
-  'http://127.0.0.1:8000/xiaomi/readings?mac=5C:85:7E:14:43:45&limit=10' | jq
-
-curl -sS -H "X-api-key: $(python3 -c 'from config import load_config; print(load_config()["api_key"])')" \
-  'http://127.0.0.1:8000/switchbot/readings?mac=EC:2E:84:06:4E:9A&limit=10' | jq
