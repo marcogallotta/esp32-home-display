cab71b439d9bf8bf3870d4307c2020934bd1a68a
Marco Gallotta <marco@gallotta.co.za>
2026-04-22 17:26:07 +0200

    API: change gen_certs to use .local

--- server/gen_certs.sh
+++ server/gen_certs.sh
@@ -1,8 +1,9 @@
 mkdir -p certs
 
-openssl req -x509 -newkey rsa:2048 \
-  -keyout certs/key.pem \
-  -out certs/cert.pem \
-  -days 365 \
-  -nodes \
-  -subj "/CN=localhost"
+openssl req -x509 \
+    -newkey rsa:2048 \
+    -keyout certs/key.pem \
+    -out certs/cert.pem \
+    -days 365 \
+    -nodes \
+    -subj "/CN=laptop.local"
