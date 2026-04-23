0e212a2ac4bbab194341f0aa49e11c32b028f7e0
Marco Gallotta <marco@gallotta.co.za>
2026-04-23 20:25:09 +0200

    ESP32: remove curl cacert which is a dupe

--- src/network_desktop.cpp
+++ src/network_desktop.cpp
@@ -27,7 +27,6 @@ TransportResult mapCurlCode(CURLcode rc) {
 
         case CURLE_SSL_CONNECT_ERROR:
         case CURLE_PEER_FAILED_VERIFICATION:
-        case CURLE_SSL_CACERT:
         case CURLE_SSL_CERTPROBLEM:
         case CURLE_SSL_ISSUER_ERROR:
             return TransportResult::TlsError;
