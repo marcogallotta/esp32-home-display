13a8e5b13fde0093ceb5e08b5a70559658045466
Marco Gallotta <marco@gallotta.co.za>
2026-05-11 07:10:51 +0200

    sb history: rm stale TODO

--- src/switchbot/history_service.h
+++ src/switchbot/history_service.h
@@ -9,13 +9,11 @@ namespace switchbot {
 namespace history {
 
 struct HistoryServiceOptions {
-    std::uint32_t newSensorWindowSeconds = 6U * 60U * 60U;
+    std::uint32_t newSensorWindowSeconds = 68U * 24U * 60U * 60U;
     std::uint32_t commandTimeoutMs = 3000;
     std::uint32_t sampleIntervalSeconds = 15U * 60U;
     std::uint32_t historyLimitSeconds = 68U * 24U * 60U * 60U;
-    // TODO: bulk upload is not enabled yet. Start with 100 readings per request,
-    // then test increasing toward the backend max of 1000.
-    std::uint32_t bulkBatchLimit = 100;
+    std::uint32_t bulkBatchLimit = 1000;
 };
 
 struct HistoryServiceState {
