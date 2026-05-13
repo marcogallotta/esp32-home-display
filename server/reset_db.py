27c5fd701e23bca2032f18369f6588ea5e7d0aa1
Marco Gallotta <marco@gallotta.co.za>
2026-05-13 16:31:40 +0200

    api: make reset_db use alembic

--- server/reset_db.py
+++ server/reset_db.py
@@ -1,3 +1,6 @@
+from alembic import command
+from alembic.config import Config as AlembicConfig
+
 from config import load_config
 from db import build_engine
 from models import Base
@@ -6,7 +9,9 @@ from models import Base
 def main():
     engine = build_engine(load_config())
     Base.metadata.drop_all(engine)
-    Base.metadata.create_all(engine)
+
+    alembic_cfg = AlembicConfig("alembic.ini")
+    command.upgrade(alembic_cfg, "head")
     print("database reset complete")
 
 
